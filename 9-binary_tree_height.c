adbc4553b85355627fdd87bbe59b57f8d925b505
maiyo008 <tonymaiyo008@gmail.com>
2023-03-02 17:59:44 +0000

    Task 9 corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,19 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-     size_t height_left, height_right;
+	size_t left_size = 0;
+	size_t right_size = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (!tree)
+		return (0);
 
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+	if (tree->left)
+		left_size += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_size += 1 + binary_tree_height(tree->right);
 
-    return (1 + (height_left > height_right ? height_left : height_right));
+	if (left_size > right_size)
+		return (left_size);
+	else
+		return (right_size);
 }
